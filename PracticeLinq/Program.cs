24522774b88e596d4f8c4ccef4e9728bcfc9e777
cristianespinosa7621@gmail.com <email@example.com>
2022-06-21 12:26:54 -0700

    intitial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>()
+            {
+                "Fifa 22",
+                "Call of Duty",
+                "GTA 5"
+            };
+
+            var ordered = videoGames.OrderBy(name => name.Length).ToList();
+        }
+    }
+}
+
